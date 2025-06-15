0eaf057efc3b47102d9681bf6b0c28b028e142dd
sergey.irin <sergey.irin@inn.ru>
2025-06-15 21:56:45 +0400

    Added learning rate schedule to AdamW

--- cs336_basics/adamw.py
+++ cs336_basics/adamw.py
@@ -1,5 +1,7 @@
 import torch
 
+from .learning_rate_schedule import learning_rate_schedule
+
 
 class AdamW(torch.optim.Optimizer):
 
@@ -9,7 +11,11 @@ class AdamW(torch.optim.Optimizer):
         lr: float = 1e-3,
         betas: tuple[float, float] = (0.9, 0.999),
         eps: float = 1e-8,
-        weight_decay: float = 1e-2
+        weight_decay: float = 1e-2,
+        lr_max: float | None = None,
+        lr_min: float | None = None,
+        t_w: int | None = None,
+        t_c: int | None = None,
     ):
         defaults = dict(
             lr=lr,
@@ -18,9 +24,12 @@ class AdamW(torch.optim.Optimizer):
             weight_decay=weight_decay,
         )
         super().__init__(params, defaults)
+        self.lr_max = lr_max
+        self.lr_min = lr_min
+        self.t_w = t_w
+        self.t_c = t_c
 
     def step(self):
-        loss = None
         for group in self.param_groups:
             lr = group["lr"]
             beta1, beta2 = group["betas"]
@@ -43,6 +52,16 @@ class AdamW(torch.optim.Optimizer):
                 state["step"] += 1
                 t = state["step"]
 
+                if self.lr_max is not None and self.lr_min is not None and \
+                    self.t_w is not None and self.t_c is not None:
+                    lr = learning_rate_schedule(
+                        t, 
+                        lr_max=self.lr_max,
+                        lr_min=self.lr_min,
+                        t_w=self.t_w,
+                        t_c=self.t_c
+                    )
+
                 # Update the first moment estimate
                 m.mul_(beta1).add_(grad, alpha=1 - beta1)
                 # Update the second moment estimate
@@ -54,4 +73,3 @@ class AdamW(torch.optim.Optimizer):
                 # Apply weight decay
                 if weight_decay != 0:
                     p.data.add_(p.data, alpha=-lr * weight_decay)
-        return loss
